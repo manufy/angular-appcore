cbd6a31ab06542af030e16f2ca5ca208052e1f69
manufy <manu@tao.homeunix.net>
2019-11-18 12:05:04 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+/* Core */ 
+
+import { CoreModule } from './core/core.module'; /* Poner ruta relativa al modulo, sin .ts */
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, CoreModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
