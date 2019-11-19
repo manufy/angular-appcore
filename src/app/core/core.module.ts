36114453afbec46ca16621b22e08c1cdedfa3d5a
manufy <manu@tao.homeunix.net>
2019-11-19 10:08:30 +0100

    3 files modified router added to shell component instead app component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
+
+/* Componentes header, main, footer */
+
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
 import { FooterComponent } from './shell/footer/footer.component';
@@ -12,7 +15,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
   exports: [ShellComponent, RouterModule]
