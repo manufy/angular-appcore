df8d5ed877b707723842f3db208fdc50ed04580f
manufy <manu@tao.homeunix.net>
2019-11-19 10:18:38 +0100

    4 files modified, 4 added - added now component in shared-infrastructure

---
+++ src/app/shared-infrastructure/shared-infrastructure.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NowComponent } from './now/now.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NowComponent]
+})
+export class SharedInfrastructureModule { }
