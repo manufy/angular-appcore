7073cc3632b455e0af1082cae0fa1603e5670437
manufy <manu@tao.homeunix.net>
2019-11-19 10:29:18 +0100

    2 files modified, 4 added - added 1-temporal module and component

---
+++ src/app/1-temporal/temporal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemporalComponent } from './temporal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TemporalComponent]
+})
+export class TemporalModule { }
